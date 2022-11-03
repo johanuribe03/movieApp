bf65e103f0812cea4cd11a3f2cbd1379915c856e
Johan Uribe <49119190+johanuribe03@users.noreply.github.com>
2022-11-02 22:09:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+// const APIKEY = '04c35731a5ee918f014970082a0088b1';
+const APIURL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1';
+const IMGPATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI =
+  'https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query='
+
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+//Initially get fav movies
+getMovies(APIURL);
+
+
+async function getMovies(url) {
+  const resp = await fetch(url);
+  const respData = await resp.json();
+
+  showMovies(respData.results);
+}
+
+function showMovies(movies) {
+  //clear main
+  main.innerHTML = '';
+
+  movies.forEach(movie => { 
+    const { poster_path, title, vote_average } = movie;
+
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
+
+    movieEl.innerHTML = `
+      <img 
+        src="${IMGPATH + poster_path}"
+        alt="${title}"
+        />
+      <div class="movie-info">
+        <h3>${title}</h3>
+        <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+      </div>
+    `;
+    
+    main.appendChild(movieEl);
+  });
+}
+
+function getClassByRate(vote) {
+  if(vote >= 8) {
+    return 'green';
+  } else if (vote >= 5) {
+    return 'orange'
+  } else {
+    return 'red'
+  }
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const searchTerm = search.value;
+  
+  if(searchTerm) {
+    getMovies(SEARCHAPI + searchTerm);
+
+
+    search.value = '';
+  }
+});
+
+
+
+
+// async function getMovies() {
+//   const resp = await fetch(APIURL);
+//   const respData = await resp.json();
+
+//   console.log(respData);
+
+//   //lets add images to the body
+//  respData.results.forEach((movie) =>{
+//   const img = document.createElement('img');
+//   img.src =IMGPATH +movie.poster_path;
+
+//   document.body.appendChild(img);
+
+
+//  });
+
+//   return respData;
+// }
+
+// getMovies();
+
+
+
+{/* <img 
+src="https://image.tmdb.org/t/p/w1280/AeyiuQUUs78bPkz18FY3AzNFF8b.jpg"
+/> */}
+
+//you should hide your API keys
+
+
+// async function getMovies() {
+//   const resp = await fetch(APIURL);
+//   const respData = await resp.json();
+
+//   console.log(respData);
+
+//   respData.results.forEach(movie => { 
+//         const { poster_path, title, vote_average } = movie;
+    
+//         const movieEl = document.createElement('div');
+//         movieEl.classList.add('movie');
+    
+//         movieEl.innerHTML = `
+//           <img 
+//             src="${IMGPATH + poster_path}"
+//             alt="${title}"
+//             />
+//           <div class="movie-info">
+//             <h3>${title}</h3>
+//             <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+//           </div>
+//         `;
+        
+//         main.appendChild(movieEl);
+//       });
+
+//   return respData;
+// }
+
+
+// function showMovies(movies) {
+//   respData.results.forEach(movie => { 
+//     const { poster_path, title, vote_average } = movie;
+
+//     const movieEl = document.createElement('div');
+//     movieEl.classList.add('movie');
+
+//     movieEl.innerHTML = `
+//       <img 
+//         src="${IMGPATH + poster_path}"
+//         alt="${title}"
+//         />
+//       <div class="movie-info">
+//         <h3>${title}</h3>
+//         <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+//       </div>
+//     `;
+    
+//     main.appendChild(movieEl);
+//   });
+// }
